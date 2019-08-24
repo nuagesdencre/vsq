c184cf580f1783d5acc162ec50449af4a10e0d54
Veronique Savard <veronique.savard@verizonmedia.com>
2019-08-24 20:10:00 +0100

    integrated router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import VueRouter from 'vue-router';
 
+//app
+import App from './App.vue'
+
+// components
+import Profile from './components/Profile.vue';
+import HelloWorld from "./components/HelloWorld";
+
+//plugins and utils
 Vue.use(Vuetify);
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+    {path: '/', component: HelloWorld},
+    {path: '/profile', component: Profile},
+];
+const router = new VueRouter({routes});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+    router,
+}).$mount('#app');
